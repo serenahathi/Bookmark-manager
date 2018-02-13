17aec7b772b6eedbdf003bcd04f18be3314400dc
kotpsot <zygmunt.justyna@gmail.com>
2018-02-13 18:44:20 +0000

    write notes on the ex 10

--- lib/link.rb
+++ lib/link.rb
@@ -9,12 +9,6 @@ class Link
   end
 
   def self.create(hash)
-    # if ENV['ENVIRONMENT'] == "test"
-    #   connection = PG.connect(dbname: 'bookmark_manager_test')
-    # else
-    #   connection = PG.connect(dbname: 'bookmark_manager')
-    # end
-    # connection.exec("INSERT INTO links (url) VALUES('#{hash[:url]}')")
     DatabaseConnection.query("INSERT INTO links (url) VALUES('#{hash[:url]}')")
   end
 
